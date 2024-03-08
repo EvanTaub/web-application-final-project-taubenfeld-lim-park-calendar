1a3dd5edfe6196aaafae5b94d92db2b2426ce3eb
Bow-Pixels <bowpixels@gmail.com>
2024-03-08 10:26:08 -0500

    Made Base Html Jinja and added a few routes

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, flash, url_for, request, redirect
+import re
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/calendar")
+def calendar():
+    return render_template("calendar.html")
+
+
+
+
+# edit / add / profile 
 
 if __name__ == "__main__":
     app.secret_key = "super_secret_key"  # Change this to a random, secure key
