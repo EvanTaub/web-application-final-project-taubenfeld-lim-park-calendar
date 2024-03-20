f50189577ba8bd8cacf039afa6f16fce5a08aba8
Bow-Pixels <bowpixels@gmail.com>
2024-03-20 10:44:11 -0400

    Login Page, Extra Imports (For Later) and User DB

--- app.py
+++ app.py
@@ -1,7 +1,59 @@
-from flask import Flask, render_template, flash, url_for, request, redirect
-import re
-import flask_login
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
+import os, io
+from werkzeug.utils import secure_filename
+import csv
+from sqlalchemy import desc, asc
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+import secrets
+from flask_mail import Mail
+from flask_mail import Message
+from flask_login import LoginManager, UserMixin
+from flask_login import login_user, current_user, logout_user, login_required
+import random
+# twilio test
+from twilio.rest import Client
+import base64, io
+from PIL import Image
+
+
+
+
+
+
 app = Flask(__name__)
+
+# Setup the database
+app["SQLALCHEMY_DATABASE_URI"] = "sqlite:///calendar.db"
+db = SQLAlchemy(app)
+
+
+
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    first_name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique = True, nullable = False)
+    password_hash = db.Column(db.String(255), nullable = False)
+    email_verification_token = db.Column(db.String(255))
+    is_verified = db.Column(db.Boolean, default = False)
+    mfa_enabled = db.Column(db.Boolean, default = False)
+    phone_number = db.Column(db.String(10), nullable = False)
+    account_type = db.Column(db.String(255), nullable = False, default = "Student")
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self,password):
+        return check_password_hash(self.password_hash, password)
+    
+    def __repr__(self):
+        return f"ID: {self.id}\n First Name: {self.first_name}\n Last Name: {self.last_name}\n Email: {self.email}\n Account Type: {self.account_type} "
+
 
 
 @app.route("/")
@@ -25,6 +77,10 @@ def register():
 
 @app.route("/login")
 def login():
+    if request.method == "GET":
+        return render_template('login.html')
+    if request.method == "POST":
+        email = request.form.get('email')
     return render_template("login.html")
 
 @app.route("/calendar")
